8229a844880bdee5dc1cc888986e7c2ce3b03a02
HeekyungKim6424 <human09129@sookmyung.ac.kr>
2021-08-02 17:00:51 +0900

    no message

---
+++ register.php
@@ -0,0 +1,10 @@
+
+    <?php
+        $email = $_POST["email"];
+        $pw = $_POST["pw"];
+    
+        echo "<p>가입이 완료 되었습니다.</p>";
+        echo "<br/><span>이메일: </span>" .$email;
+        echo "<br/><span>비밀번호: </span>" .$pw;
+    
+    ?>   
